21422c6fda41ff32ee6a518f5694479a8e34769b
c032 <git@c032.dev>
2024-06-07 17:04:53 +0000

    Extract slog-related code to `slog.go`

---
+++ slog.go
@@ -0,0 +1,113 @@
+package logger
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"runtime"
+	"slices"
+	"sync"
+	"time"
+)
+
+type slogWrapper struct {
+	mu     sync.Mutex
+	ctx    context.Context
+	logger *slog.Logger
+}
+
+func (sw *slogWrapper) Print(v ...interface{}) {
+	sw.mu.Lock()
+	defer sw.mu.Unlock()
+
+	h := sw.logger.Handler()
+
+	t := time.Now().UTC()
+	level := slog.LevelInfo
+	msg := fmt.Sprint(v...)
+
+	var pcs [1]uintptr
+	runtime.Callers(2, pcs[:])
+
+	r := slog.NewRecord(t, level, msg, pcs[0])
+	_ = h.Handle(sw.ctx, r)
+}
+
+func (sw *slogWrapper) Printf(format string, v ...interface{}) {
+	sw.mu.Lock()
+	defer sw.mu.Unlock()
+
+	h := sw.logger.Handler()
+
+	t := time.Now().UTC()
+	level := slog.LevelInfo
+	msg := fmt.Sprintf(format, v...)
+
+	var pcs [1]uintptr
+	runtime.Callers(2, pcs[:])
+
+	r := slog.NewRecord(t, level, msg, pcs[0])
+	_ = h.Handle(sw.ctx, r)
+}
+
+func (sw *slogWrapper) Error(v ...interface{}) {
+	sw.mu.Lock()
+	defer sw.mu.Unlock()
+
+	h := sw.logger.Handler()
+
+	t := time.Now().UTC()
+	level := slog.LevelError
+	msg := fmt.Sprint(v...)
+
+	var pcs [1]uintptr
+	runtime.Callers(2, pcs[:])
+
+	r := slog.NewRecord(t, level, msg, pcs[0])
+	_ = h.Handle(sw.ctx, r)
+}
+
+func (sw *slogWrapper) Errorf(format string, v ...interface{}) {
+	sw.mu.Lock()
+	defer sw.mu.Unlock()
+
+	h := sw.logger.Handler()
+
+	t := time.Now().UTC()
+	level := slog.LevelError
+	msg := fmt.Sprintf(format, v...)
+
+	var pcs [1]uintptr
+	runtime.Callers(2, pcs[:])
+
+	r := slog.NewRecord(t, level, msg, pcs[0])
+	_ = h.Handle(sw.ctx, r)
+}
+
+func (sw *slogWrapper) WithFields(fields Fields) Logger {
+	sw.mu.Lock()
+	defer sw.mu.Unlock()
+
+	l := sw.logger
+
+	var keys []string
+	for k, _ := range fields {
+		keys = append(keys, k)
+	}
+	slices.Sort(keys)
+
+	for _, k := range keys {
+		v := fields[k]
+
+		l = l.With(k, v)
+	}
+
+	return FromSlog(l)
+}
+
+func FromSlog(slogger *slog.Logger) Logger {
+	return &slogWrapper{
+		ctx:    context.Background(),
+		logger: slogger,
+	}
+}
--- standard.go
+++ standard.go
@@ -1,14 +1,8 @@
 package logger
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"log/slog"
-	"runtime"
-	"slices"
-	"sync"
-	"time"
 )
 
 var (
@@ -42,107 +36,5 @@ func (sw *standardWrapper) WithFields(fields Fields) Logger {
 func FromStandard(stdLogger *log.Logger) Logger {
 	return &standardWrapper{
 		logger: stdLogger,
-	}
-}
-
-type slogWrapper struct {
-	mu     sync.Mutex
-	ctx    context.Context
-	logger *slog.Logger
-}
-
-func (sw *slogWrapper) Print(v ...interface{}) {
-	sw.mu.Lock()
-	defer sw.mu.Unlock()
-
-	h := sw.logger.Handler()
-
-	t := time.Now().UTC()
-	level := slog.LevelInfo
-	msg := fmt.Sprint(v...)
-
-	var pcs [1]uintptr
-	runtime.Callers(2, pcs[:])
-
-	r := slog.NewRecord(t, level, msg, pcs[0])
-	_ = h.Handle(sw.ctx, r)
-}
-
-func (sw *slogWrapper) Printf(format string, v ...interface{}) {
-	sw.mu.Lock()
-	defer sw.mu.Unlock()
-
-	h := sw.logger.Handler()
-
-	t := time.Now().UTC()
-	level := slog.LevelInfo
-	msg := fmt.Sprintf(format, v...)
-
-	var pcs [1]uintptr
-	runtime.Callers(2, pcs[:])
-
-	r := slog.NewRecord(t, level, msg, pcs[0])
-	_ = h.Handle(sw.ctx, r)
-}
-
-func (sw *slogWrapper) Error(v ...interface{}) {
-	sw.mu.Lock()
-	defer sw.mu.Unlock()
-
-	h := sw.logger.Handler()
-
-	t := time.Now().UTC()
-	level := slog.LevelError
-	msg := fmt.Sprint(v...)
-
-	var pcs [1]uintptr
-	runtime.Callers(2, pcs[:])
-
-	r := slog.NewRecord(t, level, msg, pcs[0])
-	_ = h.Handle(sw.ctx, r)
-}
-
-func (sw *slogWrapper) Errorf(format string, v ...interface{}) {
-	sw.mu.Lock()
-	defer sw.mu.Unlock()
-
-	h := sw.logger.Handler()
-
-	t := time.Now().UTC()
-	level := slog.LevelError
-	msg := fmt.Sprintf(format, v...)
-
-	var pcs [1]uintptr
-	runtime.Callers(2, pcs[:])
-
-	r := slog.NewRecord(t, level, msg, pcs[0])
-	_ = h.Handle(sw.ctx, r)
-}
-
-func (sw *slogWrapper) WithFields(fields Fields) Logger {
-	sw.mu.Lock()
-	defer sw.mu.Unlock()
-
-	l := sw.logger
-
-	var keys []string
-	for k, _ := range fields {
-		keys = append(keys, k)
-	}
-	slices.Sort(keys)
-
-	for _, k := range keys {
-		v := fields[k]
-
-		l = l.With(k, v)
-	}
-
-	return FromSlog(l)
-}
-
-func FromSlog(slogger *slog.Logger) Logger {
-	return &slogWrapper{
-		ctx:    context.Background(),
-		logger: slogger,
 	}
 }
